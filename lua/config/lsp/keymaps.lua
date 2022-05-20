6960f03dae6bb17d6f996c3ba6ecefa70a58b537
aaron <yandli@hotmail.com>
2022-05-20 16:33:04 +0800

    add toggle diagnostic keymap

--- lua/config/lsp/keymaps.lua
+++ lua/config/lsp/keymaps.lua
@@ -11,6 +11,27 @@ local function keymaps(client, bufnr)
   keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>")
   keymap("n", "[e", "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.ERROR})<CR>")
   keymap("n", "]e", "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.ERROR})<CR>")
+
+  -- toggle buffer diagnostic
+  local buf_diagnostics_active = true
+  vim.keymap.set("n", "<leader>bd", function()
+    buf_diagnostics_active = not buf_diagnostics_active
+    if buf_diagnostics_active then
+      vim.diagnostic.show(nil, 0)
+    else
+      vim.diagnostic.hide(nil, 0)
+    end
+  end)
+  -- toggle workspace diagnostic
+  local diagnostics_active = true
+  vim.keymap.set("n", "<leader>bd", function()
+    diagnostics_active = not diagnostics_active
+    if diagnostics_active then
+      vim.diagnostic.show()
+    else
+      vim.diagnostic.hide()
+    end
+  end)
 end
 
 function M.setup(client, bufnr)
