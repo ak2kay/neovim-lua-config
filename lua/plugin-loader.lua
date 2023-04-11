f8657714fb76af1b0e948547b073aeb815fd0828
dahufa <yandli@hotmail.com>
2023-04-11 23:42:36 +0800

    refine lazy load

--- lua/plugin-loader.lua
+++ lua/plugin-loader.lua
@@ -1,11 +1,10 @@
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if vim.fn.isdirectory(lazypath) == 0 then
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
    vim.notify("Installing lazy ...", vim.log.levels.INFO, { title = "lazy.nvim" })
-   vim.fn.system { "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath }
-   vim.fn.system { "git", "-C", lazypath, "checkout", "tags/stable" } -- last stable release
+   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+      lazypath })
    vim.notify("Lazy installed")
 end
-
 vim.opt.rtp:prepend(lazypath)
 
 local ok, lazy = pcall(require, "lazy")
